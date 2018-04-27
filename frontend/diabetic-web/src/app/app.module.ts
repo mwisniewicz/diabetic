a97948c67863d0d60f5eeb4f67951037f686d94e
mwisniewicz <m.wisniewicz@gmail.com>
2018-04-27 11:48:04 +0200

    Added login form

--- frontend/diabetic-web/src/app/app.module.ts
+++ frontend/diabetic-web/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,8 +25,11 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     AppRoutingModule
   ],
   providers: [],
--- frontend/diabetic-web/src/app/login/login.component.ts
+++ frontend/diabetic-web/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: FormGroup;
+  loading: boolean;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.userLogin = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+   }
 
   ngOnInit() {
+    this.loading = false;
+    // this.userLogin = this.fb.group({
+    //   username: ['', Validators.required],
+    //   password: ['', Validators.required]
+    // });
   }
 
+  onLogin() {
+    this.loading = true;
+  }
 }
